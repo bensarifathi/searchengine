986acd786fabcffe24cb63e499e338406cbd2a87
BENSARI-Fathi <fethibensari@gmail.com>
2024-01-08 15:30:25 +0100

    feat: small changes

--- src/main/java/com/finalprojectdaar/searchengine/models/Book.java
+++ src/main/java/com/finalprojectdaar/searchengine/models/Book.java
@@ -5,6 +5,16 @@ public class Book {
     private String author;
     private String image;
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    private String title;
+
     public Integer getId() {
         return id;
     }
